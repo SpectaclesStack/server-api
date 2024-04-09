7de7900829f293176f5bb11f56d6fe9003187672
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-04-09 15:24:28 +0200

    modified db_connection string format in program.cs

--- SpectaclesStack.Api/Program.cs
+++ SpectaclesStack.Api/Program.cs
@@ -16,9 +16,7 @@ var databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME")?.ToString
 var username = Environment.GetEnvironmentVariable("USERNAME")?.ToString();
 var password = Environment.GetEnvironmentVariable("PASSWORD")?.ToString();
 
-var connectionString = "Server="+serverName+";Port=5432;Database="+databaseName+";Username="+username+";Password="+password+";";
-
-System.Console.WriteLine("connecting to " + connectionString);
+var connectionString = "Server="+serverName+";Port=5432;Database="+databaseName+";Username="+username+";Password="+password;
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
