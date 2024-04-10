9b47ad3895d0d6ebd8a60b8e035ad5eda94e81c2
hleketaniDuncan97 <duncanb@bbd.co.za>
2024-04-10 11:33:29 +0200

    Update api

--- SpectaclesStack.Api/obj/Debug/net8.0/spectaclesStackServer.AssemblyInfo.cs
+++ SpectaclesStack.Api/obj/Debug/net8.0/spectaclesStackServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spectaclesStackServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5c78c01d98ec059abaefc9f59ef19efe40340f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d50071fec64f9479da7dc90b6de21aa527da4d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("spectaclesStackServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spectaclesStackServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
